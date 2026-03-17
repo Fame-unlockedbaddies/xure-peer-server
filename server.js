bb3f8b1a997075c8aaea5cea13b811d8ede2cbba
Fame-unlockedbaddies <hoeniggerkys@proton.me>
2026-03-17 23:05:20 +0000

    Update server.js

--- server.js
+++ server.js
@@ -1,27 +1,7 @@
 const express = require('express');
 const { ExpressPeerServer } = require('peer');
-
 const app = express();
-
-app.get('/', (req, res) => {
-  res.send('Xure PeerJS Server is running.');
-});
-
-const server = app.listen(process.env.PORT || 3000, () => {
-  console.log('Xure PeerJS server started on port', process.env.PORT || 3000);
-});
-
-const peerServer = ExpressPeerServer(server, {
-  path: '/',
-  allow_discovery: false,
-});
-
+app.get('/', (req, res) => res.send('Xure PeerJS Server is running.'));
+const server = app.listen(process.env.PORT || 3000);
+const peerServer = ExpressPeerServer(server, { path: '/' });
 app.use('/peerjs', peerServer);
-
-peerServer.on('connection', (client) => {
-  console.log('Peer connected:', client.getId());
-});
-
-peerServer.on('disconnect', (client) => {
-  console.log('Peer disconnected:', client.getId());
-});
